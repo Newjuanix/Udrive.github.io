4cebf92323e39ea54f1fc89ae8870c78f99b8389
Newjuanix <newjuanix@gmail.com>
2025-05-06 23:04:41 -0500

    Add files via upload

---
+++ src/App.tsx
@@ -0,0 +1,38 @@
+
+import { Toaster } from "@/components/ui/toaster";
+import { Toaster as Sonner } from "@/components/ui/sonner";
+import { TooltipProvider } from "@/components/ui/tooltip";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Index from "./pages/Index";
+import DriverSignup from "./pages/DriverSignup";
+import RiderSignup from "./pages/RiderSignup";
+import Safety from "./pages/Safety";
+import Business from "./pages/Business";
+import AboutUs from "./pages/AboutUs";
+import NotFound from "./pages/NotFound";
+
+const queryClient = new QueryClient();
+
+const App = () => (
+  <QueryClientProvider client={queryClient}>
+    <TooltipProvider>
+      <Toaster />
+      <Sonner />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Index />} />
+          <Route path="/driver-signup" element={<DriverSignup />} />
+          <Route path="/rider-signup" element={<RiderSignup />} />
+          <Route path="/safety" element={<Safety />} />
+          <Route path="/business" element={<Business />} />
+          <Route path="/about" element={<AboutUs />} />
+          {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </BrowserRouter>
+    </TooltipProvider>
+  </QueryClientProvider>
+);
+
+export default App;
